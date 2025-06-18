b53dd8b9a70afeaaccecce8949f2ad02dd7c62cf
Mihir2811 <135440831+Mihir2811@users.noreply.github.com>
2025-06-18 13:44:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,4 +15,4 @@ def summarize():
     return render_template('result.html', summary=summary, original=input_text)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=7860)
